1914b431bbaea941d2befa2f1b4e6f9bc1eb1015
Manu <manuel@softpoint.es>
2023-04-09 19:21:40 +0200

    :tada: v1.00

---
+++ certgen_manual.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ] || [ -z "$2" ]
+  then
+    echo "$0 your_domain your_email"
+    exit 1
+fi
+
+
+docker-compose -f docker-compose-certbot.yml run --service-ports certbot certonly -a manual  --rsa-key-size 4096 --email "$2" -d "$1"
